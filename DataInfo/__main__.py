cf7d55d5ddb01e56370ce854d215a39e32a776ca
D Prakash <54624250+DPrakashMewari@users.noreply.github.com>
2021-09-05 02:26:34 +0530

    Add files via upload

---
+++ DataInfo/__main__.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+class Datainfo:
+    def __init__(self,filename):
+        self.filename = filename
+        file=pd.read_csv(self.filename)
+        print("File Shape :",file.shape)
+        print("-----" * 4)
+        print("File Size :",file.size)
+        print("-----" * 4)
+        count = [file[x].value_counts() for x in file.columns]
+        print("File Columns Frequency:",count)
+        print("-----" * 4)
+        print("Data Types Info:",file.info())
+        print("-----"*4)
+        print("Total Null Values Are",file.isna().sum())
+        print("-----" * 4)
+        unique = [file[x].value_counts().nunique() for x in file.columns]
+        print("Unique value in Different Columns",unique)
+        print("------" * 4)
+        print("Data Description",file.describe())
+
+
+
+def main(file):
+    obj=Datainfo(file)
+
+if __name__ == "__main__":
+    main('iris.csv')
---
+++ readme.md
@@ -0,0 +1,15 @@
+## INFO
+It is the demo Package created for learning purpose, it has class that will Show Basics About Data 
+
+## Installation 
+```pip install DataInfo ```
+
+## How to Use it ?
+Open terminal or cmd type DataInfo
+And then pass the csv name
+
+## License
+
+@ 2021 Prakash Mewari
+
+This is repo is licensed under the MIT license.
